97c7b2e1d0d9c56a4e44cb9556efb07cde0280d6
Aurume <aurelija35@gmail.com>
2019-10-07 10:34:01 +0300

    plikas mavenas

---
+++ src/main/java/bandymas/TreniruoteApplication.java
@@ -0,0 +1,13 @@
+package bandymas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreniruoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreniruoteApplication.class, args);
+	}
+
+}
